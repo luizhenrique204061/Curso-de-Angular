f7fc38d01d0b9f7a7edb6a410f660be6f4035b6f
Luiz Henrique <luizhenrique204061@gmail.com>
2026-07-09 02:49:13 -0300

    Trigésimo Segundo Commit

---
+++ Sessão 6 Projeto Filtro de Listas de Usuários/filtro-de-usuarios/src/app/app-module.ts
@@ -0,0 +1,18 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { ComponentsModule } from './components/components-module';
+
+@NgModule({
+  declarations: [App],
+  imports: [
+    BrowserModule, 
+    AppRoutingModule, 
+    ComponentsModule,
+  ],
+  providers: [provideBrowserGlobalErrorListeners()],
+  bootstrap: [App],
+})
+export class AppModule {}
